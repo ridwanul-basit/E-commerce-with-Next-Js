61bda129b17556f07b88749b2c6ac0a6c164e402
Ridwanul Basit <basitridwanul@gmail.com>
2025-11-15 12:29:43 +0600

    Sidebar Done

--- components/Application/admin/AppSidebar.jsx
+++ components/Application/admin/AppSidebar.jsx
@@ -1,16 +1,15 @@
+'use client'
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarMenuSub,
   SidebarMenuSubItem,
+  useSidebar,
 } from "@/components/ui/sidebar"
 import Image from "next/image"
 import logoBlack from '@/public/assets/images/logo-black.png'
@@ -23,13 +22,14 @@ import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/component
 import Link from "next/link"
 
 const AppSidebar = () => {
+    const {toggleSidebar} = useSidebar()
   return (
      <Sidebar className='z-50'>
         <SidebarHeader className='border-b h-14 p-0'>
             <div className="flex justify-between items-center px-4">
                 <Image src={logoBlack.src} height={50} width={logoBlack.width} alt="Logo Dark" className="block dark:hidden h-[50px] w-auto " />
                 <Image src={logoWhite.src} height={50} width={logoWhite.width} alt="Logo Light" className="hidden dark:block h-[50px] w-auto " />
-                <Button type='button' size='icon'  >
+                <Button onClick={toggleSidebar} type='button' size='icon' className='md:hidden'  >
                     <IoMdClose/>
                 </Button>
             </div>
--- components/Application/admin/TopBar.jsx
+++ components/Application/admin/TopBar.jsx
@@ -1,12 +1,15 @@
+'use client'
 import React from 'react'
 import ThemeSwitch from './ThemeSwitch'
 import UserDropdown from './UserDropdown'
 import { Button } from '@/components/ui/button'
 import { RiMenu4Fill } from "react-icons/ri";
+import { useSidebar } from '@/components/ui/sidebar';
 
 const TopBar = () => {
+    const {toggleSidebar} = useSidebar()
   return (
-    <div className='fixed border h-14  w-full top-0 left-0 z-30 md:ps-72 md:pe-10 flex justify-between items-center bg-white dark:bg-card '>
+    <div className='fixed border h-14  w-full top-0 left-0 z-30 md:ps-72 md:pe-10 px-5 flex justify-between items-center bg-white dark:bg-card '>
       <div>
       search
       </div>
@@ -14,7 +17,7 @@ const TopBar = () => {
       <div className='flex items-center gap-2'>
            <ThemeSwitch/>
            <UserDropdown/>
-           <Button type='button' size='icon' className='ms-2 '>
+           <Button onClick={toggleSidebar} type='button' size='icon' className='ms-2 md:hidden '>
             <RiMenu4Fill/>
            </Button>
       </div>
