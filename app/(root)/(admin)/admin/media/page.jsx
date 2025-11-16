06fb2b46ee045a1881c273ed06fdbebf1455b992
Ridwanul Basit <basitridwanul@gmail.com>
2025-11-16 12:23:57 +0600

    Breadcrumb installed

--- app/(root)/(admin)/admin/media/page.jsx
+++ app/(root)/(admin)/admin/media/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MediaPage = () => {
   return (
     <div>
-      
+      media
     </div>
   )
 }
--- routes/AdminPanelRoute.js
+++ routes/AdminPanelRoute.js
@@ -1,1 +1,4 @@
 export const ADMIN_DASHBOARD = '/admin/dashboard'
+
+// Media routes
+export const ADMIN_MEDIA_SHOW = '/admin/media'
