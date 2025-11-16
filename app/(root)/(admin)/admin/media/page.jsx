1b57bec2bcaa53dec6824cd8cb9c6431b9ec772f
Ridwanul Basit <basitridwanul@gmail.com>
2025-11-16 12:58:06 +0600

    Upload Media

--- app/(root)/(admin)/admin/media/page.jsx
+++ app/(root)/(admin)/admin/media/page.jsx
@@ -1,9 +1,16 @@
+import BreadCrumb from '@/components/Application/admin/BreadCrumb'
+import { ADMIN_DASHBOARD, ADMIN_MEDIA_SHOW } from '@/routes/AdminPanelRoute'
 import React from 'react'
 
 const MediaPage = () => {
+
+    const breadcrumbData = [
+        {href: ADMIN_DASHBOARD, label: 'Home' },
+        {href: '', label: 'Media' },
+    ]
   return (
     <div>
-      media
+      <BreadCrumb breadcrumbData={breadcrumbData}/>
     </div>
   )
 }
---
+++ components/Application/admin/UploadMedia.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UploadMedia = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UploadMedia
