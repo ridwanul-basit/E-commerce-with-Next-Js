17a56dd6f413ec7613bfd275f4fead3160941b5b
Ridwanul Basit <basitridwanul@gmail.com>
2025-11-27 12:38:29 +0600

    Table on going

--- app/(root)/(admin)/admin/category/page.jsx
+++ app/(root)/(admin)/admin/category/page.jsx
@@ -1,9 +1,48 @@
+import BreadCrumb from '@/components/Application/admin/BreadCrumb';
+import DatatableWrapper from '@/components/Application/admin/DatatableWrapper';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { ADMIN_CATEGORY_ADD, ADMIN_CATEGORY_SHOW, ADMIN_DASHBOARD, ADMIN_TRASH } from '@/routes/AdminPanelRoute';
+import Link from 'next/link';
 import React from 'react'
+import { FiPlus } from 'react-icons/fi';
+
+
+const breadcrumbData = [
+  { href: ADMIN_DASHBOARD, label: "Home" },
+  { href: ADMIN_CATEGORY_SHOW, label: "Category" },
+
+];
 
 const ShowCategory = () => {
   return (
     <div>
-      
+      <BreadCrumb breadcrumbData={breadcrumbData}  />
+       <Card className="py-0 rounded shadow-sm">
+        <CardHeader className="pt-3 py-2 px-3 border-b [.border-b]:py-2 ">
+         <div className='flex justify-between items-center'>
+          <h4 className='text-xl font-semibold'>Show Category</h4>
+         <Button >
+          <FiPlus/>
+          <Link href={ADMIN_CATEGORY_ADD}>New Cattegory</Link>
+         </Button>
+         </div>
+        </CardHeader>
+        <CardContent className='pb-5'>
+         <DatatableWrapper
+         queryKey = "category-data"
+         fetchUrl = '/api/category'
+         intitialPageSize={10}
+         columnsConfig={}
+         exportEndPoint={'/api/category/export'}
+         deleteEndPoint='/api/category/delete'
+         deleteType="SD"
+         trashView={`${ADMIN_TRASH}?.trashof=category`}
+         createAction= {action}
+         
+         />
+        </CardContent>
+      </Card> 
     </div>
   )
 }
--- routes/AdminPanelRoute.js
+++ routes/AdminPanelRoute.js
@@ -9,3 +9,8 @@ export const ADMIN_MEDIA_EDIT = (id)=> id?  `/admin/media/edit/${id}` : ''
 export const ADMIN_CATEGORY_ADD = '/admin/category/add'
 export const ADMIN_CATEGORY_SHOW = '/admin/category'
 export const ADMIN_CATEGORY_EDIT = (id)=> id?  `/admin/category/edit/${id}` : ''
+
+
+// Trash route
+
+export const ADMIN_TRASH = '/admin/trash'
