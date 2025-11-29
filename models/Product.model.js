07abc350cca320d61637cfede354022d5cdd7c03
Ridwanul Basit <basitridwanul@gmail.com>
2025-11-29 11:35:27 +0600

    Product model

---
+++ models/Product.model.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+import { lowercase } from "zod";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique:true,
+      lowercase:true,
+      trim: true
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Category',
+        required:true
+    },
+    mrp: {
+      type: Number,
+      required: true,
+    },
+    sellingPrice: {
+      type: Number,
+      required: true,
+    },
+    discountPercentage: {
+      type: Number,
+      required: true,
+    },
+    media:[
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Media',
+        required:true
+        }
+    ],
+    description: {
+      type: String,
+    },
+    deletedAt: {
+      type: Date,
+      default: null,
+      index: true
+    }
+  },
+  { timestamps: true }
+);
+
+// TTL index to auto-delete expired OTPs
+productSchema.index({ category: 1 })
+
+const ProductModel = mongoose.models.Product || mongoose.model("Product", productSchema, "products");
+
+export default ProductModel;
