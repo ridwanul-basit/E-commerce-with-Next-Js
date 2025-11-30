8908c009ad7fc8f56df38e56aac9d87f177c2b08
Ridwanul Basit <basitridwanul@gmail.com>
2025-11-30 15:36:39 +0600

    Export Done

--- app/api/product/export/route.js
+++ app/api/product/export/route.js
@@ -1,30 +1,35 @@
 import { isAuthenticated } from "@/lib/authentication";
 import { connectDB } from "@/lib/db";
 import { catchError, response } from "@/lib/helperFunction";
-import CategoryModel from "@/models/Category.model";
+import ProductModel from "@/models/Product.model";
+
 
 export async function GET(request) {
   try {
-       const auth = await isAuthenticated('admin',request)
-    if (!auth.isAuth){
-        return response(false,403,'Unauthorized')
+    const auth = await isAuthenticated('admin', request);
+    if (!auth.isAuth) {
+      return response(false, 403, 'Unauthorized');
     }
+
     await connectDB();
 
     const filter = {
       deletedAt: null,
     };
 
-    const getCategory = await CategoryModel.findOne(filter)
+    // Return all products as an array
+    const getProducts = await ProductModel.find(filter)
+      .select('-media -description') // exclude large fields
       .sort({ createdAt: -1 })
       .lean();
 
-    if (!getCategory) {
-      return response(false, 404, "Collection empty");
+    if (!getProducts || !getProducts.length) {
+      return response(false, 404, 'No products found');
     }
 
-    return response(true, 200, "Data Found", getCategory);
+    return response(true, 200, 'Data Found', getProducts);
   } catch (error) {
     return catchError(error);
   }
 }
+
