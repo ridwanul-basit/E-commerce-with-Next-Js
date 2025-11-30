04792783ad34107cbe82d618d06bdf6640517302
Ridwanul Basit <basitridwanul@gmail.com>
2025-11-30 15:27:32 +0600

    Product table

---
+++ app/api/product/route.js
@@ -0,0 +1,110 @@
+import { isAuthenticated } from "@/lib/authentication";
+import { connectDB } from "@/lib/db";
+import { catchError } from "@/lib/helperFunction";
+import ProductModel from "@/models/Product.model";
+
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  try {
+    const auth = await isAuthenticated("admin", request);
+    if (!auth.isAuth) {
+      return response(false, 403, "Unauthorized");
+    }
+
+    await connectDB();
+    const searchParams = request.nextUrl.searchParams;
+    const start = parseInt(searchParams.get("start") || 0, 10);
+    const size = parseInt(searchParams.get("size") || 0, 10);
+    const filters = JSON.parse(searchParams.get("filters") || "[]");
+    const globalFilter = searchParams.get("globalFilter") || "";
+    const sorting = JSON.parse(searchParams.get("sorting") || "[]");
+    const deleteType = searchParams.get("deleteType");
+
+    let matchQuery = {};
+
+    if (deleteType === "SD") {
+      matchQuery = { deletedAt: null };
+    } else if (deleteType === "PD") {
+      matchQuery = { deletedAt: { $ne: null } };
+    }
+
+    if (globalFilter) {
+      matchQuery["$or"] = [
+        { name: { $regex: globalFilter, $options: "i" } },
+        { slug: { $regex: globalFilter, $options: "i" } },
+        {"categoryData.name" : { $regex: globalFilter, $options: "i" } },
+        { $expr: { $regexMatch: { input: { $toString: "$mrp" }, regex: globalFilter, options: "i" } } },
+    { $expr: { $regexMatch: { input: { $toString: "$sellingPrice" }, regex: globalFilter, options: "i" } } },
+    { $expr: { $regexMatch: { input: { $toString: "$discountPercentage" }, regex: globalFilter, options: "i" } } },
+      ];
+    }
+
+    //column filteration
+
+    filters.forEach((filter) => {
+  // Check if the filter field is numeric
+  if (["mrp", "sellingPrice", "discountPercentage"].includes(filter.id)) {
+    // Convert filter value to number
+    matchQuery[filter.id] = Number(filter.value);
+  } else {
+    // Keep regex for string fields
+    matchQuery[filter.id] = { $regex: filter.value, $options: "i" };
+  }
+});
+
+    // sorting
+
+    let sortQuery = {};
+    sorting.forEach((sort) => {
+      sortQuery[sort.id] = sort.desc ? -1 : 1;
+    });
+
+    // aggregate pipeline
+
+    const aggregatePipeline = [
+      {
+        $lookup: {
+          from: "categories",
+          localField: "category",
+          foreignField: "_id",
+          as: "categoryData",
+        },
+      },
+      { $unwind: { path: "$categoryData", preserveNullAndEmptyArrays: true } },
+      { $match: matchQuery },
+      { $sort: Object.keys(sortQuery).length ? sortQuery : { createdAt: -1 } },
+      { $skip: start },
+      { $limit: size },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          slug: 1,
+          mrp: 1,
+          sellingPrice: 1,
+          discountPercentage: 1,
+          category: "$categoryData.name",
+          createdAt: 1,
+          updatedAt: 1,
+          deletedAt: 1,
+        },
+      },
+    ];
+
+    // Execute query
+
+    const getProduct = await ProductModel.aggregate(aggregatePipeline);
+    // get total row count
+
+    const totalRowCount = await ProductModel.countDocuments(matchQuery);
+
+    return NextResponse.json({
+      success: true,
+      data: getProduct,
+      meta: { totalRowCount },
+    });
+  } catch (error) {
+    return catchError(error);
+  }
+}
