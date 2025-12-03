1302f1e1b4e7658a85909cfeaf9601e3b9c0a050
Ridwanul Basit <basitridwanul@gmail.com>
2025-12-03 17:21:47 +0600

    Search frond end input done

--- components/Application/admin/AdminSearch.jsx
+++ components/Application/admin/AdminSearch.jsx
@@ -1,8 +1,21 @@
+import { Input } from '@/components/ui/input'
 import React from 'react'
+import { IoSearchSharp } from "react-icons/io5";
 
 const AdminSearch = () => {
   return (
-    <div>
+    <div className='md:w-[350px]'>
+        <div className='flex justify-between items-center relative'>
+            <Input
+            readOnly
+            className='rounded-full cursor-pointer'
+            placeholder='Search...'
+            
+            />
+          <button type='button' className='absolute right-3 cursor-default'>
+            <IoSearchSharp />
+          </button>
+        </div>
       
     </div>
   )
--- components/Application/admin/TopBar.jsx
+++ components/Application/admin/TopBar.jsx
@@ -5,13 +5,14 @@ import UserDropdown from './UserDropdown'
 import { Button } from '@/components/ui/button'
 import { RiMenu4Fill } from "react-icons/ri";
 import { useSidebar } from '@/components/ui/sidebar';
+import AdminSearch from './AdminSearch'
 
 const TopBar = () => {
     const {toggleSidebar} = useSidebar()
   return (
     <div className='fixed border h-14  w-full top-0 left-0 z-30 md:ps-72 md:pe-10 px-5 flex justify-between items-center bg-white dark:bg-card '>
       <div>
-      search
+     <AdminSearch/>
       </div>
       <div className='flex items-center gap-2'>
            <ThemeSwitch/>
