da4c2699eab9f0584790b16379730b7c59f050bb
Ridwanul Basit <basitridwanul@gmail.com>
2025-12-03 17:47:55 +0600

    Search Model ongoing

--- components/Application/admin/AdminSearch.jsx
+++ components/Application/admin/AdminSearch.jsx
@@ -1,8 +1,11 @@
+'use client'
 import { Input } from '@/components/ui/input'
-import React from 'react'
+import React, { useState } from 'react'
 import { IoSearchSharp } from "react-icons/io5";
+import SearchModel from './SearchModel';
 
 const AdminSearch = () => {
+    const [open,setOpen] = useState(false)
   return (
     <div className='md:w-[350px]'>
         <div className='flex justify-between items-center relative'>
@@ -10,13 +13,14 @@ const AdminSearch = () => {
             readOnly
             className='rounded-full cursor-pointer'
             placeholder='Search...'
+            onClick={()=>setOpen(true)}
             
             />
           <button type='button' className='absolute right-3 cursor-default'>
             <IoSearchSharp />
           </button>
         </div>
-      
+      <SearchModel open={open} setOpen={setOpen} />
     </div>
   )
 }
--- components/Application/admin/SearchModel.jsx
+++ components/Application/admin/SearchModel.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
-const SearchModel = () => {
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+const SearchModel = ({open,setOpen}) => {
   return (
-    <div>
-      
-    </div>
+    <Dialog open={open} onOpenChange={()=> setOpen(!open)}  >
+  
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Quick Search</DialogTitle>
+      <DialogDescription>
+       Find and navigate to any admin section instantly. Type a keyword to get started
+      </DialogDescription>
+    </DialogHeader>
+  </DialogContent>
+</Dialog>
   )
 }
 
