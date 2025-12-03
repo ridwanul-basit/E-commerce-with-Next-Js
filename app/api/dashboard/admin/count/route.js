ce1785d9fb89db2faaabf01f58d2560565c369a7
Ridwanul Basit <basitridwanul@gmail.com>
2025-12-03 14:33:28 +0600

    Admin dashboard Route ongoing

--- app/api/dashboard/admin/count/route.js
+++ app/api/dashboard/admin/count/route.js
@@ -1,5 +1,14 @@
+import { isAuthenticated } from "@/lib/authentication";
+
 export async function GET() {
     try {
+
+    const auth = await isAuthenticated("admin", request);
+        if (!auth.isAuth) {
+          return response(false, 403, "Unauthorized");
+        }
+    
+    
         
     } catch (error) {
         
