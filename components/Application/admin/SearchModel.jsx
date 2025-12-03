e38e7b74fdf4574b5edbcd26eba96d69b36d99e5
Ridwanul Basit <basitridwanul@gmail.com>
2025-12-03 18:04:54 +0600

    Search vales details

--- components/Application/admin/SearchModel.jsx
+++ components/Application/admin/SearchModel.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import {
   Dialog,
   DialogContent,
@@ -7,7 +8,10 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import { Input } from '@/components/ui/input'
+import Link from 'next/link'
 const SearchModel = ({open,setOpen}) => {
+    const [query,setQuery] = useState('')
   return (
     <Dialog open={open} onOpenChange={()=> setOpen(!open)}  >
   
@@ -18,6 +22,28 @@ const SearchModel = ({open,setOpen}) => {
        Find and navigate to any admin section instantly. Type a keyword to get started
       </DialogDescription>
     </DialogHeader>
+<Input 
+placeholder="Search..."
+value={query}
+onChange={(e)=> setQuery(e.target.value)}
+autoFocus
+/>
+
+<ul className='mt-4 max-h-60 overflow-y-auto'>
+    <li>
+        <Link href=''  
+        className='block py-2 px-3 rounded hover:bg-muted'
+        >
+
+        <h4 className='font-medium' >Title</h4>
+        <p className='text-sm text-muted-foreground'>Lorem ipsum dolor sit amet.</p>
+        
+        </Link>
+    </li>
+
+
+</ul>
+
   </DialogContent>
 </Dialog>
   )
