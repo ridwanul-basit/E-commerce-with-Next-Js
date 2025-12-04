29dd5ad6a4cec9b391c648bf33a45ee9b030195b
Ridwanul Basit <basitridwanul@gmail.com>
2025-12-04 12:10:55 +0600

    Kumb google font set on website

--- app/(root)/(website)/layout.jsx
+++ app/(root)/(website)/layout.jsx
@@ -1,12 +1,21 @@
 import Footer from '@/components/Application/Website/Footer'
 import Header from '@/components/Application/Website/Header'
 import React from 'react'
+import { Kumbh_Sans } from 'next/font/google'
+
+const kumbh = Kumbh_Sans({
+  weight : ["400","500","600","700","800"],
+  display : 'swap',
+  subsets : ['latin']
+})
 
 const layout = ({children}) => {
   return (
-    <div>
+    <div className={kumbh.className}>
       <Header/>
-      {children}
+      <main>
+        {children}
+      </main>
       <Footer/>
     </div>
   )
