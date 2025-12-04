eba0269b3d1a7d63dd9a4c6f0ac47958afcadda2
Ridwanul Basit <basitridwanul@gmail.com>
2025-12-04 12:07:33 +0600

    Website layout Created

--- app/(root)/(website)/layout.jsx
+++ app/(root)/(website)/layout.jsx
@@ -1,9 +1,13 @@
+import Footer from '@/components/Application/Website/Footer'
+import Header from '@/components/Application/Website/Header'
 import React from 'react'
 
 const layout = ({children}) => {
   return (
     <div>
+      <Header/>
       {children}
+      <Footer/>
     </div>
   )
 }
