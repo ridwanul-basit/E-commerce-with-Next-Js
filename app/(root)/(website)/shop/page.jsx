7f092d28265854b1799dc7f3e3218e34f5c2432f
Ridwanul Basit <basitridwanul@gmail.com>
2025-12-10 15:42:01 +0600

    Filter page

--- app/(root)/(website)/shop/page.jsx
+++ app/(root)/(website)/shop/page.jsx
@@ -1,9 +1,29 @@
+import WebsiteBreadCrumb from '@/components/Application/Website/WebsiteBreadCrumb'
+import { WEBSITE_SHOP } from '@/routes/WebsiteRoute'
 import React from 'react'
+
+
+const breadcrumb = {
+    title : 'Shop',
+    links: [
+        {
+            label : "Shop" , href : WEBSITE_SHOP
+        }
+    ]
+}
 
 const ShopPage = () => {
   return (
     <div>
-      Shop
+      <WebsiteBreadCrumb props={breadcrumb} />
+      <section className='lg:flex g:px-32 px-4 my-20 ' >
+            <div className='w-72 me-4' >
+                <div className='sticky top-0 bg-gray-50 p-4 rounded'>
+                    
+                </div>
+
+            </div>
+      </section>
     </div>
   )
 }
