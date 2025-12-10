d56ffc6eb8dc061da155629806d5868a81f06751
Ridwanul Basit <basitridwanul@gmail.com>
2025-12-10 16:02:31 +0600

    Color api set

---
+++ app/api/product-variant/colors/route.js
@@ -0,0 +1,25 @@
+import { connectDB } from "@/lib/db";
+import { catchError, response } from "@/lib/helperFunction";
+import ProductVariantModel from "@/models/ProductVariant.model";
+
+
+
+export async function GET() {
+  try {
+   
+    await connectDB();
+
+    const getParams = await params;
+    const id = getParams.id;
+
+    const getColor = await ProductVariantModel.distinct('color')
+
+    if (!getColor) {
+      return response(false, 404, "Color not found");
+    }
+
+    return response(true, 200, "Color Found", getColor);
+  } catch (error) {
+    return catchError(error);
+  }
+}
