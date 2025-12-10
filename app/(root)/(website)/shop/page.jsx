9dab0b671bf093fccc5ac54716e62969821ba9cd
Ridwanul Basit <basitridwanul@gmail.com>
2025-12-10 18:35:11 +0600

    Shadcn slider

--- app/(root)/(website)/shop/page.jsx
+++ app/(root)/(website)/shop/page.jsx
@@ -1,32 +1,31 @@
-import Filter from '@/components/Application/Website/Filter'
-import WebsiteBreadCrumb from '@/components/Application/Website/WebsiteBreadCrumb'
-import { WEBSITE_SHOP } from '@/routes/WebsiteRoute'
-import React from 'react'
-
+import Filter from "@/components/Application/Website/Filter";
+import WebsiteBreadCrumb from "@/components/Application/Website/WebsiteBreadCrumb";
+import { WEBSITE_SHOP } from "@/routes/WebsiteRoute";
+import React from "react";
 
 const breadcrumb = {
-    title : 'Shop',
-    links: [
-        {
-            label : "Shop" , href : WEBSITE_SHOP
-        }
-    ]
-}
+  title: "Shop",
+  links: [
+    {
+      label: "Shop",
+      href: WEBSITE_SHOP,
+    },
+  ],
+};
 
 const ShopPage = () => {
   return (
     <div>
       <WebsiteBreadCrumb props={breadcrumb} />
-      <section className='lg:flex lg:px-32 px-4 my-20 ' >
-            <div className='w-72 me-4' >
-                <div className='sticky top-0 bg-gray-50 p-4 rounded'>
-                    <Filter />
-                </div>
-
-            </div>
+      <section className="lg:flex lg:px-32 px-4 my-20 ">
+        <div className="w-72 me-4">
+          <div className="sticky top-0 bg-gray-50 p-4 rounded">
+            <Filter />
+          </div>
+        </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default ShopPage
+export default ShopPage;
--- components/Application/Website/WebsiteBreadCrumb.jsx
+++ components/Application/Website/WebsiteBreadCrumb.jsx
@@ -1,32 +1,36 @@
-import React from 'react'
-import pageTitle from "@/public/assets/images/page-title.png"
-import Link from 'next/link'
-import { WEBSITE_HOME } from '@/routes/WebsiteRoute'
+import React from "react";
+import pageTitle from "@/public/assets/images/page-title.png";
+import Link from "next/link";
+import { WEBSITE_HOME } from "@/routes/WebsiteRoute";
 
-const WebsiteBreadCrumb = ({props}) => {
+const WebsiteBreadCrumb = ({ props }) => {
   return (
-    <div className='py-10 flex justify-center items-center bg-[url("/assets/images/page-title.png")] bg-cover bg-center  ' >
+    <div className='py-10 flex justify-center items-center bg-[url("/assets/images/page-title.png")] bg-cover bg-center  '>
       <div>
-        <h1 className='text-2xl font-semibold mb-2 text-center' >{props.title}</h1>
-        <ul className='flex gap-2 justify-center'>
-            <li><Link href={WEBSITE_HOME} className='font-semibold' />Home</li>
-            {props.links.map((item,index)=> (
-                <li key={index}>
-                    <span className='me-1'>/</span>
-                    {item.href?
-                      <Link href={item.href} >{item.label} </Link>    
-                      :
+        <h1 className="text-2xl font-semibold mb-2 text-center">
+          {props.title}
+        </h1>
+        <ul className="flex gap-2 justify-center">
+          <li>
+            <Link href={WEBSITE_HOME} className="font-semibold">
+              Home
+            </Link>
+          </li>
 
-                      <span>{item.label}</span>
-                }
-                    
-                </li>
-            ))}
-
+          {props.links.map((item, index) => (
+            <li key={index}>
+              <span className="me-1">/</span>
+              {item.href ? (
+                <Link href={item.href}>{item.label} </Link>
+              ) : (
+                <span>{item.label}</span>
+              )}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WebsiteBreadCrumb
+export default WebsiteBreadCrumb;
