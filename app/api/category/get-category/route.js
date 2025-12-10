382bd0e1a08b5a3ac9329dfe0ace3fc63cb2e115
Ridwanul Basit <basitridwanul@gmail.com>
2025-12-10 15:48:00 +0600

    get Category for user route set

---
+++ app/api/category/get-category/route.js
@@ -0,0 +1,24 @@
+import { connectDB } from "@/lib/db";
+import { catchError, response } from "@/lib/helperFunction";
+import CategoryModel from "@/models/Category.model";
+
+
+export async function GET() {
+  try {
+   
+    await connectDB();
+
+    const getParams = await params;
+    const id = getParams.id;
+
+    const getCategory = await CategoryModel.find({deletedAt: null}).lean();
+
+    if (!getCategory) {
+      return response(false, 404, "Category not found");
+    }
+
+    return response(true, 200, "Category Found", getCategory);
+  } catch (error) {
+    return catchError(error);
+  }
+}
