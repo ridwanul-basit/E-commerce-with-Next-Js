eb1704d41e7251a8492482ab6928ab6b94289948
Ridwanul Basit <basitridwanul@gmail.com>
2025-12-10 15:53:04 +0600

    Category fetched

--- components/Application/Website/Filter.jsx
+++ components/Application/Website/Filter.jsx
@@ -1,12 +1,15 @@
+"use client"
+import useFetch from '@/hooks/useFetch'
 import React from 'react'
 
 const Filter = () => {
    
-    
+    const {data:categoryData} = useFetch('/api/category/get-category')
+
 
   return (
     <div>
-      
+      Filter
     </div>
   )
 }
